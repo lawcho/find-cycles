f47f1bb208d780dcacbaf309e62a833eac111a40
lawcho <95857153+lawcho@users.noreply.github.com>
2023-10-27 17:32:49 +0200

    Support all the OSes

--- flake.nix
+++ flake.nix
@@ -1,20 +1,16 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      all_ghc_versions = nixpkgs.lib.attrNames nixpkgs.legacyPackages.${system}.haskell.packages;
-    in
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs , flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+    let all_ghc_versions = nixpkgs.lib.attrNames nixpkgs.legacyPackages.${system}.haskell.packages; in
     {
-      packages.${system} = (
-        # Build the an output for each version of GHC in the nixpkgs snapshot
-        nixpkgs.lib.attrsets.genAttrs all_ghc_versions (ghc_version:
-          # Use 'cabal build' to compile the Haskell
-          # (but with GHC & haskell libraries from the nixpkgs snapshot)
-          nixpkgs.legacyPackages.${system}.haskell.packages.${ghc_version}.developPackage {
-            root = ./.;
-          }
-        )
-      );
-    };
+      # Build an output for each version of GHC in the nixpkgs snapshot
+      packages = nixpkgs.lib.attrsets.genAttrs all_ghc_versions (ghc_version:
+        # Use 'cabal build' to compile the Haskell
+        # (but with GHC & haskell libraries from the nixpkgs snapshot)
+        nixpkgs.legacyPackages.${system}.haskell.packages.${ghc_version}.developPackage {
+          root = ./.;
+        });
+    }));
 }
