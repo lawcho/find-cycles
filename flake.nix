5e689e585a22c50a163f26a0d6bf06268f18c8f2
lawcho <95857153+lawcho@users.noreply.github.com>
2023-10-27 17:38:11 +0200

    Tidy up flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs , flake-utils }:
+  outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
-    let all_ghc_versions = nixpkgs.lib.attrNames nixpkgs.legacyPackages.${system}.haskell.packages; in
+    let haskell_packages = nixpkgs.legacyPackages.${system}.haskell.packages; in
     {
       # Build an output for each version of GHC in the nixpkgs snapshot
-      packages = nixpkgs.lib.attrsets.genAttrs all_ghc_versions (ghc_version:
+      packages = nixpkgs.lib.attrsets.genAttrs (nixpkgs.lib.attrNames haskell_packages) (ghc_version:
         # Use 'cabal build' to compile the Haskell
-        # (but with GHC & haskell libraries from the nixpkgs snapshot)
-        nixpkgs.legacyPackages.${system}.haskell.packages.${ghc_version}.developPackage {
+        # (but with GHC & Haskell libraries from the nixpkgs snapshot)
+        haskell_packages.${ghc_version}.developPackage {
           root = ./.;
         });
     }));
