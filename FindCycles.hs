f75d9e0e3f19de77e868adc633129dea1181e8ae
lawcho <95857153+lawcho@users.noreply.github.com>
2023-12-06 16:16:43 +0100

    Add view of the condensation graph

--- FindCycles.hs
+++ FindCycles.hs
@@ -9,19 +9,23 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
 
-import Algebra.Graph.AdjacencyMap (AdjacencyMap, edgeList, vertexList)
+import Algebra.Graph.AdjacencyMap (AdjacencyMap, edgeList, vertexList, gmap)
 import qualified Algebra.Graph.AdjacencyMap as AdjacencyMap
 import qualified Algebra.Graph.Acyclic.AdjacencyMap as Acyclic
 import qualified Algebra.Graph.NonEmpty.AdjacencyMap as NonEmpty
-import Control.Monad (forM_, when)
+import Control.Monad (forM_, when, unless)
 import Data.Aeson (ToJSON)
 import Data.Aeson.Text (encodeToLazyText)
 import Data.FileEmbed (embedStringFile,makeRelativeToProject)
+import Data.List (elemIndex,sort)
+import qualified Data.List.NonEmpty as NonEmpty
 import Data.Text.Lazy (Text)
 import qualified Data.Text.Lazy.IO as Text
 import Text.InterpolatedString.Perl6
 import Data.Functor ((<&>))
 import Data.IORef (newIORef)
+import qualified Data.Map as Map
+import Data.Map ((!))
 import Data.Maybe (fromJust)
 import qualified Data.Set as Set
 import GHC.Generics (Generic)
@@ -69,17 +73,48 @@ main = do
         g <- getOrCreateGraph (hieDir opts) db
         -- Get Strongly Connected Components,
         -- i.e. clumps of mutually recursive defintions
-        let clumps = Acyclic.scc g
-        -- Keep only SCCs that cross modules
-        let sccs = Acyclic.vertexSet . Acyclic.induce
-                    (not . allInSameModule . Set.toList . NonEmpty.vertexSet) $ clumps
+        let scc_graph' = gmap (NonEmpty.fromNonEmpty) . Acyclic.fromAcyclic . Acyclic.scc $ g
+        -- There are too many SCCs to render them all, so remove all the single-module SCCs
+        let scc_graph = AdjacencyMap.induce (not . allInSameModule . vertexList) scc_graph'
+        let scc_list = (`zip` [1..]) . vertexList $ scc_graph
+        let getSccName = \scc -> "scc" ++ show (Map.fromList scc_list ! scc)
+        let getSccFile = \scc -> getSccName scc ++ ".html"
         -- Print out analysis
-        putStrLn ("There are " ++ (show . Set.size) sccs ++ " cross-module SCCs in the code.")
+        putStrLn $ "There are " ++ (show . length) scc_list ++ " cross-module SCCs in the code."
+        putStrLn "Writing out viewers for cross-module SCCs..."
         hFlush stdout
-        forM_ ([1..] `zip` Set.toList sccs) $ \(n,scc) -> do
-            let json = encodeToLazyText . toD3 v2fqident v2ident v2module . NonEmpty.fromNonEmpty $ scc
-            let filepath = "scc" ++ show n ++ ".html"
-            Text.writeFile filepath [qq|
+        forM_ scc_list $ \(scc,n) -> do
+            writeGraph (getSccFile scc) scc
+                v2fqident v2ident (const Nothing) v2group
+        putStrLn "Writing out viewer for condensation graph..."
+        writeGraph "index.html" scc_graph
+            getSccName getSccName (Just . getSccFile) (\scc -> 
+                let scc_gnames = NonEmpty.group . sort . map v2module . vertexList $ scc
+                in map (\l -> D3Group {gname = NonEmpty.head l, amount = NonEmpty.length l}) scc_gnames)
+
+-- Parts of the HTML graph viewer widget, loaded at compiled and printed into every generated html file 
+stylesheet,controller :: Text
+stylesheet = $(makeRelativeToProject "style.css" >>= embedStringFile)
+controller = $(makeRelativeToProject "controller.js" >>= embedStringFile)
+
+-- Write out an HTML viewer widget for algebraic-graphs graph
+writeGraph :: (Eq v) => FilePath -> AdjacencyMap v
+           -> (v -> String) -> (v -> String) -> (v -> Maybe String) -> (v -> [D3Group])
+           -> IO ()
+writeGraph path g v2id v2name v2hyperlink v2groups =
+    let json = encodeToLazyText $ D3Graph
+            {   nodes = vertexList g <&> \ v -> D3Node
+                {   id = v2id v
+                ,   name = v2name v
+                ,   hyperlink = v2hyperlink v
+                ,   groups = v2groups v
+                }
+            ,   edges = edgeList g <&> \ (v1,v2) -> D3Edge
+                {   source = v2id v1
+                ,   target = v2id v2
+                }
+            }
+    in Text.writeFile path [qq|
 <!DOCTYPE html>
 <style>
 $stylesheet
@@ -91,35 +126,19 @@ $controller
 <script>
 data = $json
 </script>
-        |]
-
--- Parts of the HTML graph viewer widget, loaded at compiled and printed into every generated html file 
-stylesheet,controller :: Text
-stylesheet = $(makeRelativeToProject "style.css" >>= embedStringFile)
-controller = $(makeRelativeToProject "controller.js" >>= embedStringFile)
-
--- Convert a graph from algebraic-graphs format to the JSON format
-toD3 :: (Eq v) => (v -> String) -> (v -> String) -> (v -> String) -> AdjacencyMap v -> D3Graph
-toD3 v2id v2name v2group g = D3Graph
-    {   nodes = vertexList g <&> \ v -> D3Node
-        {   id = v2id v
-        ,   name = v2name v
-        ,   group = v2group v
-        }
-    ,   edges = edgeList g <&> \ (v1,v2) -> D3Edge
-        {   source = v2id v1
-        ,   target = v2id v2
-        }
-    }
+|]
 
 -- Data types mirroring the JSON input format to the graph viewer widget
 data D3Graph = D3Graph {nodes :: [D3Node], edges :: [D3Edge]} deriving (Generic)
-data D3Node = D3Node {id :: String, name :: String, group :: String}  deriving (Generic)
 data D3Edge = D3Edge {source :: String, target :: String} deriving (Generic)
+data D3Node = D3Node {id :: String, name :: String,
+    hyperlink :: Maybe String, groups :: [D3Group]}  deriving (Generic)
+data D3Group = D3Group {gname :: String, amount :: Int} deriving (Generic)
 
 instance ToJSON D3Graph
 instance ToJSON D3Node
 instance ToJSON D3Edge
+instance ToJSON D3Group
 
 -- Are all the vertices in a list from the same module?
 allInSameModule :: [HieDb.Vertex] -> Bool
@@ -134,6 +153,9 @@ v2fqident v = concat $
     [v2module v, ".", v2ident v , "@[", show $ v2sl v, ",", show $ v2el v,"]"]
 v2sl (_,_,_,sl,_,_,_) = sl
 v2el (_,_,_,_,_,el,_) = el
+
+v2group :: HieDb.Vertex -> [D3Group]
+v2group v = [D3Group {gname = v2module v, amount = 1}]
 
 -- Load HieDb from file (creating file if does not exist)
 getOrCreateGraph :: FilePath -> HieDb -> IO (AdjacencyMap HieDb.Vertex)
--- controller.js
+++ controller.js
@@ -10,7 +10,10 @@ function setup(graph) {
   console.log('Data loaded from JS!')
   
   const colors = d3.scaleOrdinal(d3.schemeCategory10)
-  const radius = 5
+  const min_radius = 5
+  // Constants for simulation
+  const link_rest_length = 5  // Too low => unstable, too high => no clustering
+  const charge_per_mass = 80
 
   // HTML element that nodes will be added under
   const root = d3.select("body")
@@ -46,9 +49,32 @@ function setup(graph) {
   const svg_nodes = svg_contents.append("g").attr("id","nodes")
     .selectAll()
     .data(graph.nodes)
-    .join("circle")
-    .attr("r", radius)
-    .attr("fill", d => colors(d.group))
+    .join("a")
+    .attr("href", n => n.hyperlink)
+
+
+  // Helper functions
+  const group_amount = group => group.amount // getter
+  const node_mass = node => d3.sum(node.groups.map(group_amount))
+  const node_radius = node => min_radius * Math.sqrt(node_mass(node))
+
+  // Draw each node as a pie chart (by group breakdown)
+  // (when each node has exactly one group, this just draws circles)
+  const svg_segments = svg_nodes
+    .selectAll()
+    .data(node =>
+      d3.pie()
+        .value(group_amount)(node.groups)
+        .map(seg_data => ({seg_data: seg_data, node: node}))
+      )
+    .join("path")
+    .attr("fill", segment => colors(segment.seg_data.data.gname))
+    .attr("d", segment => d3.arc()({
+      innerRadius: 0,
+      outerRadius: node_radius(segment.node),
+      startAngle: segment.seg_data.startAngle,
+      endAngle: segment.seg_data.endAngle
+    }))
   
   // Add node labels to SVG
   const svg_labels = svg_contents.append("g").attr("id","labels")
@@ -57,13 +83,16 @@ function setup(graph) {
     .data(graph.nodes)
     .join("text")
     .attr("class","label")
-  svg_labels.append("tspan").html(d => (d.name || d.id) + "\n")
+  svg_labels.append("tspan").html(n => (n.name || n.id) + "\n")
   
+  // De-duplicated list of all group names that appear in the dataset
+  const groups = [...new Set([].concat(...new Set(graph.nodes.map(n => n.groups.map(g => g.gname)))))]
+
   // Create legend
   const legend = root.append("details").attr("id","legend")
   legend.append("summary").text("Legend")
   const legend_entries = legend.selectAll()
-    .data([...new Set(graph.nodes.map(d => d.group))])
+    .data(groups)
     .join("div")
   legend_entries
     .append("svg")
@@ -78,49 +107,65 @@ function setup(graph) {
 
   // Misc. helpers
   const labels_of = n => svg_labels.filter(l => l.id == n.id)
-  const legends_of = n => legend_entries.filter(g => g == n.group)
-  const nodes_of = g => svg_nodes.filter(n => n.group == g)
+  const legends_of = segment => legend_entries.filter(gname => gname == segment.seg_data.data.gname)
+  const segments_of = gname => svg_segments.filter(segment => segment.seg_data.data.gname == gname)
   const set_class = (cls,sel,v) => sel.classed(cls, _ => v)
   const toggle_class = (cls,sel) => set_class(cls,sel,!sel.classed(cls))
 
-  // When hovering over a node, peek the label and highlight the legend
+  // When hovering over a node, peek the label
   svg_nodes
     .on("mouseover", e => d3
       .select(e.currentTarget)
       .each(n => set_class("peeking",labels_of(n), true))
-      .each(n => set_class("highlighted",legends_of(n), true))
-      .call(sel => set_class("highlighted",sel,true))
       )
     .on("mouseout", e => d3
       .select(e.currentTarget)
       .each(n => set_class("peeking",labels_of(n), false))
-      .each(n => set_class("highlighted",legends_of(n), false))
-      .call(sel => set_class("highlighted",sel,false))
       )
     // When a node is clicked, pin/unpin its label
     .on("click", e => d3
       .select(e.currentTarget)
       .each(n => toggle_class("pinned",labels_of(n)))
       )
+  
+  // When hovering over a segment, highlight its legend entry
+  svg_segments
+    .on("mouseover", e => d3
+      .select(e.currentTarget)
+      .each(n => {
+        set_class("highlighted",legends_of(n), true)
+        legends_of(n).node().scrollIntoView(true)
+        })
+      .call(sel => set_class("highlighted",sel,true))
+      )
+    .on("mouseout", e => d3
+      .select(e.currentTarget)
+      .each(n => set_class("highlighted",legends_of(n), false))
+      .call(sel => set_class("highlighted",sel,false))
+      )
 
-  // When hovering over a legend entry, highlight all its nodes
+  // When hovering over a legend entry, highlight all its segments
   legend_entries
     .on("mouseover", e => d3
         .select(e.currentTarget)
-        .each(n => set_class("highlighted",nodes_of(n), true))
+        .each(n => set_class("highlighted",segments_of(n), true))
         .call(sel => set_class("highlighted",sel,true))
         )
     .on("mouseout", e => d3
         .select(e.currentTarget)
-        .each(n => set_class("highlighted",nodes_of(n), false))
+        .each(n => set_class("highlighted",segments_of(n), false))
         .call(sel => set_class("highlighted",sel,false))
         )
 
   // Create a simulation with several forces.
   const simulation = d3
     .forceSimulation(graph.nodes)
-    .force("edges attract", d3.forceLink(graph.edges).id(d => d.id))
-    .force("nodes repel", d3.forceManyBody())
+    .force("edges attract", d3.forceLink(graph.edges).id(node => node.id)
+      // Adjust rest length of links to account for variable node radii
+      .distance(edge => link_rest_length + node_radius(edge.source) + node_radius(edge.target)))
+    .force("nodes repel", d3.forceManyBody()
+      // Big nodes repel more
+      .strength(n => -charge_per_mass * node_mass(n)))
     .force("left/right repel", d3.forceX())
     .force("top/bottom repel", d3.forceY())
     // Every simuation tick, update SVG element positions
@@ -131,8 +176,7 @@ function setup(graph) {
         .attr("x2", d => d.target.x)
         .attr("y2", d => d.target.y)
       svg_nodes
-        .attr("cx", d => d.x)
-        .attr("cy", d => d.y)
+        .attr("transform",d=>"translate("+d.x+","+d.y+")")
       svg_labels
         .attr("x", d => d.x)
         .attr("y", d => d.y)
@@ -159,5 +203,5 @@ function setup(graph) {
 
 // Credits:
 //  Using d3.js for definition dependency visualization: https://1lab.dev/
-//  Setting up d3.js: https://observablehq.com/@d3/force-directed-graph/2
-
+//  d3.js force layout: https://observablehq.com/@d3/force-directed-graph/2
+//  d3.js sized pies: https://observablehq.com/@analyzer2004/bubble-pie-chart
